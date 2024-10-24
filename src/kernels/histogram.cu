ca435eca78684165768c69d10d22a80702059109
thomas.xu <thomas.xu@epita.fr>
2024-10-24 22:51:35 +0200

    fix: histogram

--- src/kernels/histogram.cu
+++ src/kernels/histogram.cu
@@ -28,30 +28,34 @@ void compute_histogram(raft::device_span<int> buffer, raft::device_span<int> his
 }
 */
 
-
+template <int HISTO_SIZE>
 __global__ void compute_histogram_kernel(raft::device_span<int> buffer, raft::device_span<int> histo)
 {
-    int tid = threadIdx.x;
-
     extern __shared__ int shared_histo[];
-    shared_histo[tid] = 0;
+    if (threadIdx.x < HISTO_SIZE)
+        shared_histo[threadIdx.x] = 0;
     __syncthreads();
 
-    for (int i = tid; i < buffer.size(); i += blockDim.x)
-    {
-        if (buffer[i] >= 0 && buffer[i] < 256)
-        {
-            atomicAdd(&shared_histo[buffer[i]], 1);
-        }
-    }
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    if (id < buffer.size())
+        atomicAdd(&shared_histo[buffer[id]], 1);
     __syncthreads();
+    if (threadIdx.x < HISTO_SIZE)
+        atomicAdd(&histo[threadIdx.x], shared_histo[threadIdx.x]);
+}
 
-    histo[tid] = shared_histo[tid];
+__global__ void set_zeros_kernel(raft::device_span<int> histo)
+{
+    const int id = threadIdx.x;
+    histo[id] = 0;
 }
 
 void compute_histogram(raft::device_span<int> buffer, raft::device_span<int> histo, cudaStream_t stream)
 {
-    int block_size = 256;
-    compute_histogram_kernel<<<1, block_size, 256 * sizeof(int), stream>>>(buffer, histo);
+    const int histo_size = 256;
+    int block_size = 512;
+    int grid_size = (buffer.size() + block_size - 1) / block_size;
+    set_zeros_kernel<<<1, block_size, 0, stream>>>(histo);
+    compute_histogram_kernel<histo_size><<<grid_size, block_size, histo_size * sizeof(int), stream>>>(buffer, histo);
     CUDA_CHECK_ERROR(cudaGetLastError());
 }
