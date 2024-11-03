77007b10cc15ab79b7d0098f77fedfd15aea717e
thomas.xu <thomas.xu@epita.fr>
2024-11-03 17:54:17 +0100

    fix: scatter 1 global read less

--- src/kernels/scatter.cu
+++ src/kernels/scatter.cu
@@ -8,13 +8,14 @@ void scatter_kernel(raft::device_span<int> buffer, raft::device_span<int> predic
     if (id >= buffer.size())
         return;
 
-    if (buffer[id] != garbage_val)
-        res_image_buffer[predicate[id]] = buffer[id];
+    int val = buffer[id];
+    if (val != garbage_val)
+        res_image_buffer[predicate[id]] = val;
 }
 
 void scatter(raft::device_span<int> buffer, raft::device_span<int> predicate, raft::device_span<int> res_image_buffer, const int garbage_val, cudaStream_t stream) 
 {
-    int block_size = 1024;
+    int block_size = 512;
     int grid_size = (buffer.size() + block_size - 1) / block_size;
     scatter_kernel<<<grid_size, block_size, 0, stream>>>(buffer, predicate, res_image_buffer, garbage_val);
     CUDA_CHECK_ERROR(cudaGetLastError());
